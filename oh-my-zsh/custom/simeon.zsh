22fa5c7a57417b37a7057b687740846c91861d6f
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-09-24 13:38:03 -0700

    Add Ruby env vars for faster ruby

--- oh-my-zsh/custom/simeon.zsh
+++ oh-my-zsh/custom/simeon.zsh
@@ -4,6 +4,14 @@ fi
 export VISUAL="/usr/bin/vim"
 export EDITOR="/usr/bin/vim"
 
+# Faster ruby
+# @see https://gist.github.com/1688857
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=100000000
+export RUBY_HEAP_FREE_MIN=500000
+
 # Export ENV for gist cli
 export GITHUB_USER=sfw@simeonfosterwillbanks.com
 export GITHUB_PASSWORD=$(security 2>&1 >/dev/null find-generic-password -gs github.password | sed 's/password: "\(.*\)"/\1/')
