f3a165827ff7396ff42f30f38c7787cbb4943994
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-09-27 16:59:44 -0700

    for lesson planet, gcd to mirro gcm from git plugin

--- oh-my-zsh/custom/lessonplanet.zsh
+++ oh-my-zsh/custom/lessonplanet.zsh
@@ -1,5 +1,6 @@
 alias work="cd $HOME/Sites/lessonplanet"
 alias workopen="mvim $HOME/Sites/lessonplanet"
+alias gcd="git checkout development"
 alias gupd="git fetch origin && git rebase origin/development"
 alias gird="git rebase -i origin/development"
 alias migrations\?="git diff --name-only master development | ack migrate"
