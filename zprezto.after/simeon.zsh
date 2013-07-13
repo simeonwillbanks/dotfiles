bcf923665293be7c7c054075be2700356a649a7b
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-07-13 14:09:13 -0700

    no need to unalias bi since its no longer defined

--- zprezto.after/simeon.zsh
+++ zprezto.after/simeon.zsh
@@ -66,5 +66,4 @@ function zgm() {
   zeus generate migration $1 | ruby -e 'print STDIN.read.scan(/db\/migrate.*/)[0]' | pbcopy
 }
 
-unalias bi # set by bundler plugin
 alias bi='bundle install; bower install'
