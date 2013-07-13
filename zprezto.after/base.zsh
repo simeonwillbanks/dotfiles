3edfe4cbef0ca751c24216bcfe89774fed39a00a
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-07-13 14:07:07 -0700

    no need to init rbenv since prezto ruby does it for us

--- zprezto.after/base.zsh
+++ zprezto.after/base.zsh
@@ -1,6 +1,1 @@
 export PATH=/usr/local/bin:$HOME/.rbenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:$GOPATH/bin
-
-# Add rbenv init to your shell to enable shims and autocompletion
-if [[ -d ~/.rbenv ]]; then
-  eval "$(rbenv init -)"
-fi
