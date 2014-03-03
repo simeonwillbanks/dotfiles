74686334ceea4c437b58042b8cad6b1716db50c4
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-03-03 08:17:00 -0800

    Go to and open LP project

--- zprezto.after/lessonplanet.zsh
+++ zprezto.after/lessonplanet.zsh
@@ -1,5 +1,5 @@
 alias work="cd $HOME/Sites/lessonplanet"
-alias workopen="mvim $HOME/Sites/lessonplanet"
+alias workopen="$cd $HOME/Sites/lessonplanet; mvim $HOME/Sites/lessonplanet"
 alias gcod="git checkout development"
 alias gupd="git fetch origin && git rebase origin/development"
 alias gird="git rebase -i origin/development"
