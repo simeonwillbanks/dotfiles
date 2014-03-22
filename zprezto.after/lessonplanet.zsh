f1ea8e2b2991ff24b7cc9ceabe782bbb9593c0fd
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-03-22 15:17:04 -0700

    Cleanup LP aliases

--- zprezto.after/lessonplanet.zsh
+++ zprezto.after/lessonplanet.zsh
@@ -1,8 +1,6 @@
 alias work="cd $HOME/Sites/lessonplanet"
-alias workopen="$cd $HOME/Sites/lessonplanet; mvim $HOME/Sites/lessonplanet"
+alias workopen="$cd $HOME/Sites/lessonplanet; vim $HOME/Sites/lessonplanet"
 alias gcod="git checkout development"
-alias gupd="git fetch origin && git rebase origin/development"
-alias gird="git rebase -i origin/development"
-alias migrations\?="git diff --name-only master development | ack migrate"
+alias migrations\?="git diff --name-only master development | ag migrate"
 
 export LP_CODE_ROOT_PATH=/Users/simeon/Sites
