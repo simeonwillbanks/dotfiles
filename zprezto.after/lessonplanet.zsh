532a6d72514c2d2243e51a4dff1cccc5ca048ecc
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-08-11 17:00:33 -0700

    Hello Stitch Fix

--- zprezto.after/lessonplanet.zsh
+++
@@ -1,6 +0,0 @@
-alias work="cd $HOME/Sites/lessonplanet"
-alias workopen="$cd $HOME/Sites/lessonplanet; vim $HOME/Sites/lessonplanet"
-alias gcod="git checkout development"
-alias migrations\?="git diff --name-only master development | ag migrate"
-
-export LP_CODE_ROOT_PATH=/Users/simeon/Sites
