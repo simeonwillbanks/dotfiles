5892145930a7307465b3ac27bf5533ed063d4d3b
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-08-19 09:46:23 -0700

    Add minx start

--- zprezto.after/stitchfix.zsh
+++ zprezto.after/stitchfix.zsh
@@ -1,2 +1,3 @@
 alias h='heroku'
 alias es='elasticsearch --config=/usr/local/opt/elasticsearch/config/elasticsearch.yml'
+alias ms='PORT=3000 RACK_ENV=development RAILS_ENV=development WEB_CONCURRENCY=1 bundle exec foreman start'
